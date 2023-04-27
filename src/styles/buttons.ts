7b9d63f64bfbed1751a0deffc454c000b5d52881
emartinss <edumartinscmp@gmail.com>
2023-04-27 18:48:22 -0300

    add Header

---
+++ src/styles/buttons.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyleButtonForm = styled.button`
+width: 250px;
+height: 30px;
+
+background-color: rgba(0,0,0,0.5);
+color: #ffffff;
+
+border: 1px solid #000000;
+border-radius: 5px;
+
+`
