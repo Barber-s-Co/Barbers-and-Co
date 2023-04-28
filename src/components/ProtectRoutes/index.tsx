5f786b857feb7bd45edec7450c47c19a07df72af
emartinss <edumartinscmp@gmail.com>
2023-04-28 10:21:27 -0300

    feat: add routes

--- src/components/ProtectRoutes/index.tsx
+++ src/components/ProtectRoutes/index.tsx
@@ -1,1 +1,7 @@
-export const ProtectRoutes = () => {};
+import { Navigate, Outlet } from "react-router-dom";
+
+export const ProtectRoutes = () => {
+  const token = localStorage.getItem("@TOKEN");
+
+  return !token ? <Navigate to="/" /> : <Outlet />;
+};
