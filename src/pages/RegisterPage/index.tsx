1a1894d63fc479a3be67601b490a0805da8b21ae
unknown <lucasplays1414@gmail.com>
2023-04-28 12:07:35 -0300

    feat: add logo, styled register

--- src/pages/RegisterPage/index.tsx
+++ src/pages/RegisterPage/index.tsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import { RegisterForm } from "../../components/Form/RegisterForm";
+import logo from "../../assets/logoName.svg";
 
 export const RegisterPage = () => {
   return (
     <>
       <header>
-        <img src="" alt="" />
+        <img src={logo} alt="logo" />
         <Link to="/">Home</Link>
         <Link to="/">Login</Link>
       </header>
